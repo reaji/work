df6f4bf2bc70bd88042dc1d4e109cedc82dcbf3b
reaji <R@douBles-MacBook-Pro.local>
2016-08-17 17:56:33 -0700

    massive upgrade

---
+++ bubble/js/javascript.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+
+	$('.video').click(function(){
+		$('#facebookarchive').fadeOut('fast');
+		$('#facebookarchivevideo').fadeIn('slow');
+	});
+
+	$('.turnoff').click(function(){
+		$('#facebookarchive').fadeIn('slow');
+		$('#facebookarchivevideo').fadeOut('fast');
+		
+	});
+	
+
+});
