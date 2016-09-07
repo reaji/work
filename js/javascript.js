dcce75c84b962b98d0a1ed487f7eba31c128441e
reaji <R@douBles-MacBook-Pro.local>
2016-09-06 17:06:12 -0700

    new nav design

--- js/javascript.js
+++ js/javascript.js
@@ -11,6 +11,25 @@ $(document).ready(function(){
 		$('#facebookarchivevideo').fadeOut('fast');
 		
 	});
-	
 
+// 	$( window ).scroll(function() {
+// 		if( scroll = false ){
+// 		$( '#navbackground' ).css( "display", "none" );
+// 	}else{
+// 	$( '#navbackground' ).css( "box-shadow", "0 2px 2px 0 rgba(0, 0, 0, 0.1), 0 3px 4px 0 rgba(0, 0, 0, 0.15" );
+// 	}
+// });
+
+
+$(window).scroll(_.debounce(function(){
+    $('#navbackground').css("box-shadow", "rgba(0,0,0,0.16) 0 2px 4px");
+}, 150, { 'leading': true, 'trailing': false }));
+
+$(window).scroll(_.debounce(function(){
+    $('#navbackground').css("box-shadow", "none");
+}, 150));
+
+// 	$( window ).end( "scroll" ,function() {
+//   $( '#navbackground' ).css( "display", "none" );
+// });
 });
