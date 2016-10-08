30cea1405530257d8d6019fbbdc572d061ea62da
reaji <R@douBles-MacBook-Pro.local>
2016-10-08 16:33:53 -0700

    nav update

--- bubble/js/javascript.js
+++ bubble/js/javascript.js
@@ -11,6 +11,32 @@ $(document).ready(function(){
 		$('#facebookarchivevideo').fadeOut('fast');
 		
 	});
-	
 
+// 	$( window ).scroll(function() {
+// 		$( '#navbackground' ).css( "opacity", "1" );
+// });
+
+$(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#navbackground').css( "opacity", "1" );
+        } else {
+            $('#navbackground').css( "opacity", "0" );
+        }
+    });
+
+
+$(window).scroll(_.debounce(function(){
+    $('#navbackground').css("box-shadow", "rgba(0,0,0,0.16) 0 2px 4px");
+}, 150, { 'leading': true, 'trailing': false }));
+
+$(window).scroll(_.debounce(function(){
+    $('#navbackground').css("box-shadow", "none");
+}, 150));
+
+
+
+
+// 	$( window ).end( "scroll" ,function() {
+//   $( '#navbackground' ).css( "display", "none" );
+// });
 });
